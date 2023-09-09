ca287350edf52287c3de5567b7a2c47a9ea8ece9
Eli musa <60873440+unholydisaster@users.noreply.github.com>
2023-09-09 16:43:30 +0300

    Update page.jsx

--- src/app/components/harrypotter/page.jsx
+++ src/app/components/harrypotter/page.jsx
@@ -3,8 +3,8 @@ import "../../app/styles.css";
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import HarryCharacters from "../components/HarryCharacters";
-import Hero from "../components/Hero";
+import HarryCharacters from "../components/harryCharacters";
+import Hero from "../components/hero";
 
 const HarryCharactersList = () => {
   useEffect(() => {
